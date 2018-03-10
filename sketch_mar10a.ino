dd2000ba22eb25c0defe659b8fc4861e9b14452e
Mokhtari Mohammed El Amine <37108394+amine0110@users.noreply.github.com>
2018-03-10 21:17:09 +0100

    Add files via upload

---
+++ sketch_mar10a.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+Servo amine;
+
+void setup() {
+  pinMode(3 , OUTPUT);
+  pinMode(4 , OUTPUT);
+  
+  amine.attach(5);
+
+}
+
+void loop() {
+  digitalWrite(3, HIGH);
+  delay(4000);
+  digitalWrite(3, LOW);
+  delay(4000);
+  for( int x=0 ; x <= 90; x++){
+    
+    amine.write(x);
+    delay(15);
+    } 
+  
+
+}
